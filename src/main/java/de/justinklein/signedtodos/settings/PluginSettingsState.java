b7aecb3635972997ba26ab28fe62a55f94675de7
jklein <jus.klein@icloud.com>
2023-02-23 15:29:00 +0100

    feat(settings): Added State Annotation

--- src/main/java/de/justinklein/signedtodos/settings/PluginSettingsState.java
+++ src/main/java/de/justinklein/signedtodos/settings/PluginSettingsState.java
@@ -2,13 +2,19 @@ package de.justinklein.signedtodos.settings;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@State(
+  name = "de.justinklein.signedtodos.settings.PluginSettingsState",
+  storages = @Storage("SdkSettingsPlugin.xml")
+)
 public class PluginSettingsState implements PersistentStateComponent<PluginSettingsState> {
-  public String userId = "John Q. Public";
-  
+  public String userId = "Your Name Here :-)";
+
   public static PluginSettingsState getInstance() {
     return ApplicationManager.getApplication().getService(PluginSettingsState.class);
   }
